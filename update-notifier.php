13296914358869afe2605464d64bf9c9819658c3
brothman01 <brothman01@gmail.com>
2017-02-12 12:00:35 -0500

    Update update-notifier.php

--- update-notifier.php
+++ update-notifier.php
@@ -11,49 +11,40 @@
 class UpdatesNotifier {
 
 
-	public $updates;
+	public static $updates;
 
 	public function __construct() {
 
 		add_action( 'admin_bar_menu', [ $this, 'un_check_for_updates' ] );
+
+		// add the options page
+		add_action( 'admin_menu', [ $this, 'add_plugin_page' ] );
+
+		// create and add the fields to the options page
+		add_action( 'admin_init', [ $this, 'page_init' ] );
 
 	}
 
 	public function un_check_for_updates() {
 
 		if( ! current_user_can( 'install_plugins' ) ) {
+
 			return;
+
 		}
+
 			$update_data = wp_get_update_data();
 
-			$this->$updates = array(
+			self::$updates = array(
 				'plugins'	=>	$update_data['counts']['plugins'],
 				'themes'	=>	$update_data['counts']['themes'],
 				'WordPress'	=>	$update_data['counts']['themes'],
 				'translations' =>	$update_data['counts']['themes'],
 			);
 
-			print_r( $this->$updates );
+			// $this->$updates;
 
-	}
-
-}
-
-class UpdatesNotifier_Settings extends UpdatesNotifier {
-
-
-
-	public function __construct( $updates ) {
-
-				add_action( 'admin_menu', [ $this, 'add_plugin_page' ] );
-
-				print_r( 'second ' . $updates );
-
-				$this->init();
-
-	}
-
-	public function init() {
+			print_r( self::$updates);
 
 	}
 
@@ -65,8 +56,8 @@ class UpdatesNotifier_Settings extends UpdatesNotifier {
 	public function add_plugin_page() {
 
 		add_options_page(
-			__( 'Updates Settings', 'updates-notifier' ),
-			__( 'Updates Notifier', 'updates-notifier' ),
+			'Updates Notifier Settings',
+			'Updates Notifier',
 			'manage_options',
 			'updates-notifier',
 			[ $this, 'create_admin_page' ]
@@ -88,9 +79,13 @@ class UpdatesNotifier_Settings extends UpdatesNotifier {
 
 						printf(
 							'<div class="notice notice-' . $this->alert_type() . ' is-dismissible"><p>' .
-							'Plugin Updates: ' . '0' . '<br />Theme Updates: ' . '0' . '<br />WordPress Core Updates: ' . '0' . '<br />Translation Updates: ' . '0' .
+							'Plugin Updates: ' . self::$updates['plugins'] . '<br />Theme Updates: ' . self::$updates['themes'] . '<br />WordPress Core Updates: ' . self::$updates['WordPress'] . '<br />Translation Updates: ' . self::$updates['translations'] .
 							'</p></div>'
 						);
+
+						settings_fields( 'updates_notifier_settings_group' );
+
+						do_settings_sections( 'updates-notifier' );
 
 						submit_button();
 
@@ -104,12 +99,85 @@ class UpdatesNotifier_Settings extends UpdatesNotifier {
 	}
 
 	public function alert_type() {
+
 		// return 'info' or 'error'
+		if (self::$updates['plugins'] + self::$updates['themes'] + self::$updates['WordPress'] + self::$updates['translations'] == 0) {
+
 		return 'info';
+
+	} else {
+
+		return 'error';
+
+	}
+
+	}
+
+	/**
+	* Register and add settings
+	*
+	* @since 1.0.0
+	*/
+	public function page_init() {
+
+		register_setting(
+			'updates_notifier_settings_group',
+			'php_notifier_settings',
+			array( $this, 'sanitize' )
+		);
+
+		add_settings_section( 'updates-notifier-id', 'Watch These For Updates:', array( $this, 'print_section_info' ), 'updates-notifier' );
+
+		// add_settings_field( 'updates-notifier-check-plugins', $title, $callback, $page, $section, $args );
+
+	}
+
+	/**
+	* Sanitize each setting field as needed
+	*
+	* @param array $input Contains all settings fields as array keys
+	*
+	* @since 1.0.0
+	*/
+	public function sanitize( $input ) {
+
+		$new_input = [];
+
+		$new_input['warning_type']     = self::$options['warning_type'];
+		$new_input['send_email']       = (bool) empty( $input['send_email'] ) ? false : true;
+		$new_input['email_frequency']  = isset( $input['email_frequency'] ) ? sanitize_text_field( $input['email_frequency'] ) : 'Never';
+
+		if ( self::$options['email_frequency'] !== $input['email_frequency'] ) {
+
+			wp_clear_scheduled_hook( 'php_notifier_email_cron' );
+
+			if ( ! self::$options['email_frequency'] ) {
+
+				return $new_input;
+
+			}
+
+			update_option( 'php_notifier_prevent_cron', true );
+
+			wp_schedule_event( time(), $new_input['email_frequency'], 'php_notifier_email_cron' );
+
+		}
+
+		return $new_input;
+
+	}
+
+	/**
+	* Print the Section text
+	*
+	* @since 1.0.0
+	*/
+	public function print_section_info() {
+
+		echo 'Adjust the settings below:';
+
 	}
 
 }
 
 $updates_notifier = new UpdatesNotifier();
-
-$settings = new UpdatesNotifier_Settings( $updates_notifier->$updates );
