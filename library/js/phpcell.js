46c99bc23b5dc204c7ac792c347643cfed4fd49f
Ben Rothman <brothman01@gmail.com>
2017-03-28 21:16:51 -0400

    externalized phpcell as jquery

--- library/js/phpcell.js
+++ library/js/phpcell.js
@@ -1,22 +1,22 @@
 
-	document.addEventListener( "DOMContentLoaded", function( event ) {
+jQuery(document).ready(function() {
 
-		var php_action_field = document.getElementById("php_action_field");
 
 
 		setTimeout(function(){
 
 				if (wpm_data_php.state == "Up To Date") {
 
-					php_action_field.style.background = "#00CB25";
+					jQuery('#php_action_field').css( 'background',  '#00CB25' );
 
 				} else {
 
-					php_action_field.style.background = "#FF0000";
+					jQuery('#php_action_field').css( 'background', '#FF0000');
 
 				}
 
-				php_action_field.value = wpm_data_php.state;
+					jQuery('#php_action_field').val( wpm_data_php.state + '' );
+
 
 		}, 1000);
 
